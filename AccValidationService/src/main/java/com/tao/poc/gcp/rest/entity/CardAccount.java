27a583676b196b701628f53a98cf7547e53fbf91
331418 <331418@LT065126.cts.com>
2019-11-21 22:07:59 +0530

    na

--- AccValidationService/src/main/java/com/tao/poc/gcp/rest/entity/CardAccount.java
+++ AccValidationService/src/main/java/com/tao/poc/gcp/rest/entity/CardAccount.java
@@ -14,7 +14,7 @@ import javax.persistence.Table;
 public class CardAccount {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "ACC_NBR", nullable = false)
     private int accountNumber;
     
     @Column(name = "EXPIRY_DATE", nullable = true)
